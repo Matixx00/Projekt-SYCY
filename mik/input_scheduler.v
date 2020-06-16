b1c33b4cf15b0170fee8d80ea16b96f048872d0e
Mikołaj Pańka <01149377@pw.edu.pl>
2020-06-16 09:43:25 +0200

    new branch scheduler created 	new file:   mik/input_scheduler.v

---
+++ mik/input_scheduler.v
@@ -0,0 +1,17 @@
+/*
+ * by SYCY_Proj_3
+ *
+ * Scheduler for several parallel TEA synchronnized decryptors
+ *
+ *
+ */
+ 
+module input_scheduler (
+	input			clk, rst,
+			[63:0] 	in_word64,
+			
+	output	[63:0]	out_word64
+	
+);
+
+endmodule
